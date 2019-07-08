7a07b67bff453c9ac185ea5825d3eba4b5652c76
Jan Michalowsky <jan.michalowsky@axelspringer.de>
2019-07-08 16:35:25 +0200

    Merging dhax:editorialReview and bakatz:master ngs pull requests. NGS unfortunately did not respond to pull requests since 1.5 years! The project seems to be abandoned which is sad. PLEASE SEARCH A NEW MAINTAINER

--- amazon/item.go
+++ amazon/item.go
@@ -17,21 +17,22 @@ type Items struct {
 
 // Item represents item
 type Item struct {
-	XMLName         xml.Name `xml:"Item"`
-	ASIN            string
-	DetailPageURL   string
-	SalesRank       int
-	ItemLinks       ItemLinks
-	SmallImage      Image
-	MediumImage     Image
-	LargeImage      Image
-	ImageSets       ImageSets
-	ItemAttributes  ItemAttributes
-	OfferSummary    OfferSummary
-	Offers          Offers
-	CustomerReviews CustomerReviews
-	SimilarProducts SimilarProducts
-	BrowseNodes     BrowseNodes
+	XMLName          xml.Name `xml:"Item"`
+	ASIN             string
+	DetailPageURL    string
+	SalesRank        int
+	ItemLinks        ItemLinks
+	SmallImage       Image
+	MediumImage      Image
+	LargeImage       Image
+	ImageSets        ImageSets
+	ItemAttributes   ItemAttributes
+	EditorialReviews EditorialReviews
+	OfferSummary     OfferSummary
+	Offers           Offers
+	CustomerReviews  CustomerReviews
+	SimilarProducts  SimilarProducts
+	BrowseNodes      BrowseNodes
 }
 
 // ItemLinks represents ItemLinks
@@ -50,6 +51,17 @@ type Image struct {
 	URL    string
 	Height Size
 	Width  Size
+}
+
+// EditorialReviews response group
+type EditorialReviews struct {
+	EditorialReview EditorialReview
+}
+
+// EditorialReview response attribute
+type EditorialReview struct {
+	Source  string
+	Content string
 }
 
 // Size represents Size
@@ -112,6 +124,7 @@ type ItemAttributes struct {
 	EANList           EANList
 	CatalogNumberList CatalogNumberList
 	Format            []string
+	Feature           []string
 	IsAdultProduct    bool
 	ISBN              string
 	Label             string
